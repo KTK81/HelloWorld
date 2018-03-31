a9bae55cc16eee7a94f9a2956df634e3b20ba953
KTK81 <jankotek@gmail.com>
2018-03-31 21:18:54 +0200

    canvas drawImage

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+//window.onload = function (e) {
+//test();
+//}
+
 
 var canvasMain = document.getElementById("canvasFirst");
 var ctx = canvasMain.getContext("2d");
-var canvasBeta = document.getElementById("canvasSecond");
-var ctxBeta = canvasBeta.getContext("2d");
+        var canvasBeta = document.getElementById("canvasSecond");
+        var ctxBeta = canvasBeta.getContext("2d");
+
+
+
 
 var btnBlue = document.getElementById("btnColorBlue");
 var btnGreen = document.getElementById("btnColorGreen");
@@ -21,11 +28,18 @@ function drawFromExistingImage() {
     ctx.drawImage(image, canvasMain.width/2-25, canvasMain.height/2-25, 50, 50);
 }
 
+
 function drawFromFile () {
     var imageBeta = new Image ();
     imageBeta.src = "rea-office-20-20-wenge.jpg";
+    imageBeta.onload = function() {
+        ctxBeta.drawImage(imageBeta, 0, 0, canvasBeta.width, canvasBeta.height);
+    }
+
     
-    ctxBeta.drawImage(imageBeta, 0, 0, canvasBeta.width, canvasBeta.height);
+    
+    
+//    ctxBeta.drawImage(imageBeta, 0, 0, canvasBeta.width, canvasBeta.height);
     
 }
 
